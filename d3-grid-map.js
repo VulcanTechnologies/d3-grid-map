d6bed7ed13f2cc8ccd37d24c560e9cc728e856fd
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-08-21 14:41:47 -0700

    added HUD and cellID retrieval

--- d3-grid-map.js
+++ d3-grid-map.js
@@ -41,8 +41,8 @@
 
     var rows = gridSize[1];
     var cols = gridSize[0];
-    var x_size = 0.5; // this.grid_cols / 360;
-    var y_size = 0.5; //this.grid_rows / 180;
+    var x_size = 0.5; // this.gridCols / 360;
+    var y_size = 0.5; //this.gridRows / 180;
 
     var array = [];
     for (var i=1; i<=rows * cols; i++) {
@@ -65,14 +65,16 @@
     this.rotate_latitude = 0.0;
     this.rotate_longitude = 0.0;
 
-    this.grid_cols = gridSize[0];
-    this.grid_rows = gridSize[1];
+    this.gridCols = gridSize[0];
+    this.gridRows = gridSize[1];
 
     this.container = d3.select(container);
 
     var rect = this.container.node().getBoundingClientRect();
     this.width = rect.width;
     this.height = rect.height;
+
+    this.options = options;
 
     this.landColor = options.landColor || 'rgba(237,178,48,1)';
     this.seaColor = options.seaColor || 'rgba(21,98,180,.8)';
@@ -103,11 +105,19 @@
 
     this.canvas = this.container
       .append('canvas')
+      .style('position', 'absolute')
       .datum(topojson.feature(options.countries, options.countries.objects.countries))
       .attr('width', this.width)
       .attr('height', this.height);
 
+    this.hud = this.container
+      .append('canvas')
+      .style('position', 'absolute')
+      .attr('width', this.width)
+      .attr('height', this.height);
+
     this.context = this.canvas.node().getContext('2d');
+    this.hud_context = this.hud.node().getContext('2d');
     this.colorScale = options.colorScale || defaultColorScale;
 
     this.simplifyingPath = d3.geo.path()
@@ -119,7 +129,6 @@
       .context(this.context);
 
     this.init = function() {
-
       this.cellIdToCoordinates = cellIdToCoordinates(gridSize);
       this.initEvents();
       d3.select(window).on('resize', this.resize.bind(this));
@@ -127,9 +136,19 @@
     };
 
     this.destroy = function() {
-      // d3 seems to be holding onto DOM elements.  get real with it.
       this.canvas.remove();
-    }
+    };
+
+    this.coordinatesToCellId = function(coords) {
+      var lon = coords[0];
+      var lat = coords[1];
+
+      var row = ~~(this.gridRows - (lat + 90) / 180  * this.gridRows);
+      var col = ~~((lon + 180) / 360  * this.gridCols);
+
+      var cellId = row * this.gridCols + col + 1;
+      return cellId;
+    };
 
     this.initEvents = function() {
 
@@ -154,17 +173,50 @@
           self.debouncedDraw();
         })
         .on('zoom', function(d) {
+          if (zoom.scale() >= 2000 || zoom.scale() <= self.width/6) {
+            return;
+          }
           scale = d3.event.scale;
           self.area = 20000 / scale / scale;
           self.projection.scale(scale);
           self.drawWorld();
         })
         .scale(this.width/6)
-        .scaleExtent([this.width/6, 1000]);
+        .scaleExtent([this.width/6, 2000]);
 
       this.container.call(drag);
       this.container.call(zoom);
 
+      if (this.options.hud) {
+        var coordFormat = d3.format(' >+8.3f');
+
+        var fontSize = self.options.hud.fontSize || 30;
+        var verticalOffset = self.options.hud.verticalOffset || 10;
+        var fontColor = self.options.hud.fontColor || 'white';
+        var fontFace = self.options.hud.fontFace || 'monospace';
+
+        var font = fontSize + 'px ' + fontFace;
+        var h = fontSize + verticalOffset;
+        var gradient = self.hud_context.createLinearGradient(0,self.height-h,0,self.height);
+        gradient.addColorStop(0, 'rgba(0,0,0,0.0');
+        gradient.addColorStop(1, 'rgba(0,0,0,1.0');
+
+        this.container.on('mousemove', function() {
+          self.hud_context.clearRect(0,self.height-(h), self.width, self.height);
+          self.hud_context.fillStyle = gradient;
+          self.hud_context.fillRect(0,self.height-(h), self.width, self.height);
+
+          var s = '';
+          var coords = self.projection.invert(d3.mouse(this));
+          if (coords[0] && coords[1] && coords[0] > -180 && coords[0] < 180 && coords[1] > -90 && coords[1] < 90) {
+            var cellId = self.coordinatesToCellId(coords);
+            s = 'cell ID: ' + cellId + ' ( ' + coordFormat(coords[0]) + '°,' + coordFormat(coords[1]) + '° )';
+            self.hud_context.font = font;
+            self.hud_context.fillStyle = fontColor;
+            self.hud_context.fillText(s, 0, self.height - verticalOffset);
+          }
+        });
+      }
     };
 
     var graticule = d3.geo.graticule()();
@@ -206,6 +258,12 @@
 
       self.drawWorld();
 
+      self.context.beginPath();
+      self.path(self.geojson);
+      self.context.strokeStyle = self.geoJsonColor;
+      self.context.lineWidth = 0.5;
+      self.context.stroke();
+
       if (self.geojson && self.geojson.features) {
         self.geojson.features.forEach(function(feature){
           var color = null;
@@ -236,16 +294,28 @@
       };
     };
 
-    self.debouncedDraw = debounce(self.draw, 250);
+    self.debouncedDraw = debounce(self.draw, 200);
 
     this.resize = function() {
       this.width = parseInt(this.container.style('width'), 10);
       this.width = this.width - this.margin.left - this.margin.right;
       this.canvas.attr('width', this.width);
+      this.hud.attr('width', this.width);
       this.projection
         .translate([this.width/2, this.height/2])
         .clipExtent([[0, 0], [this.width, this.height]]);
       this.draw();
+    };
+
+    this.setData = function(data) {
+      if (data.constructor === ArrayBuffer) {
+        this.setDataArrayBuffer(data);
+      } else if (data.constructor === UInt8ClampedArray) {
+        this.setDataUnsparseTypedArray(data);
+      } else {
+        // assume GeoJSON
+        this.setDataGeoJSON(data);
+      }
     };
 
     this.setDataUnsparseTypedArray = function (data) {
